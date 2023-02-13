ba100b1a705fea273fa656a4a339e956e8948fd1
Ben Andrew <andr-be@github.com>
2023-02-13 10:44:14 +0000

    First remote push attempt: go!

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,3 +1,8 @@
+/// Advent of Code 2022 - Day 4 - Parts 1 & 2 Solutions
+/// andr-be 2023
+///
+///
+
 fn main() {
     println!("Hello, world!");
 }
--- day_4/target/debug/deps/day_4-a31df396c4b5c319.d
+++ day_4/target/debug/deps/day_4-a31df396c4b5c319.d
@@ -1,5 +1,5 @@
-/home/andr-be/rust_programs/advent_of_code_2022/day_4/target/debug/deps/day_4-a31df396c4b5c319.rmeta: src/main.rs
+/home/andr-be/rust_programs/Advent_of_Code_2022/day_4/target/debug/deps/day_4-a31df396c4b5c319.rmeta: src/main.rs
 
-/home/andr-be/rust_programs/advent_of_code_2022/day_4/target/debug/deps/day_4-a31df396c4b5c319.d: src/main.rs
+/home/andr-be/rust_programs/Advent_of_Code_2022/day_4/target/debug/deps/day_4-a31df396c4b5c319.d: src/main.rs
 
 src/main.rs:
