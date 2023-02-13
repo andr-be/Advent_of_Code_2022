6ab56129073725bf8fdcf18f087138e67c735735
Ben Andrew <andr-be@github.com>
2023-02-13 14:51:38 +0000

    day 4 complete

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -16,8 +16,8 @@ fn main() {
     let problem = Path::new("src/input.txt");
     let input = read_input(problem);
     let lines = input.lines();
-    let mut p1_solution = 0;
-    let mut p2_solution = 0;
+    let mut p1_solution_huw = 0;        
+    let mut p2_solution_huw = 0;
 
     for pair in lines {
         // split the pair 
@@ -35,51 +35,15 @@ fn main() {
         let p1_e = p1.next().unwrap().parse::<usize>().unwrap() + 1;
         let p2_s = p2.next().unwrap().parse::<usize>().unwrap();
         let p2_e = p2.next().unwrap().parse::<usize>().unwrap() + 1;
-        
-        // construct the range 
-        let p1_range = [p1_s..p1_e];
-        let p2_range = [p2_s..p2_e];
 
-        // convert ranges into vectors 
-        let mut p1_numbers: Vec<usize> = Vec::new();
-        let mut p2_numbers: Vec<usize> = Vec::new();
-        for number in p1_range {
-            for sub in number {
-                p1_numbers.push(sub);
-            }
+        // Huw Solution
+        if (p1_s >= p2_s && p1_e <= p2_e) || ( p2_s >= p1_s && p2_e <= p1_e) {
+            p1_solution_huw += 1;
         }
-        for number in p2_range {
-            for sub in number {
-                p2_numbers.push(sub);
-            }
-        }
-        println!("\n{pair}");
 
-        let mut not_contained_1 = 0;
-        let mut not_contained_2 = 0;
-
-        for number in &p1_numbers {
-            if ! p2_numbers.contains(&number) {
-                not_contained_1 += 1;
-            }
-        }
-        println!("Total P1 not contained in P2: {not_contained_1}");
-
-        for number in &p2_numbers {
-            if ! p1_numbers.contains(&number) {
-                not_contained_2 += 1;
-            }
-        }
-        println!("Total P2 not contained in P1: {not_contained_2}");
-
-        if not_contained_1 == 0 || not_contained_2 == 0 {
-            p1_solution += 1;
-        }
-        
-        if not_contained_1 < p1_numbers.len() || not_contained_2 < p2_numbers.len() {
-            p2_solution += 1;
+        if ( p1_s <= p2_s && p1_e > p2_s ) || ( p2_s <= p1_s && p2_e > p1_s ) {
+            p2_solution_huw +=1;
         }
     }   
-
-    println!("P1 ANSWER: {p1_solution}\nP2 ANSWER: {p2_solution}");
+    println!("HUW SOLUTION\tP1: {p1_solution_huw}\tP2: {p2_solution_huw}");
 }
