9a59bd78a898fde95720e42bb5e4b7a4eb02e7ad
Ben Andrew <andr-be@github.com>
2023-02-21 21:04:56 +0000

    day 6 perfected

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,7 +1,7 @@
 /// Advent of Code 2022 - Day 6 - Parts 1 & 2 Solutions
 /// andr-be 2023
-///
-use std::{path::Path, collections::HashMap};
+/// this is an exceptionally elegant solution
+use std::{collections::HashMap, path::Path};
 
 fn read_input(path: &Path) -> String {
     match std::fs::read_to_string(path) {
@@ -10,33 +10,27 @@ fn read_input(path: &Path) -> String {
     }
 }
 
-fn main() {
-    let path = Path::new("src/input.txt");
+fn find_answer(path: &Path, window_size: usize) -> usize {
+    let windows = read_input(path).chars().collect::<Vec<char>>();
+    let windows = windows.windows(window_size).enumerate();
 
-    let input = read_input(path);
-
-    let character_vec: Vec<char> = input.chars().collect();
-    println!("Input Length: {}", character_vec.len());
-
-    let window_size = 14;
-    let windows = character_vec.windows(window_size).enumerate();
-    
     for (slice_no, window) in windows {
         let mut hash_map = HashMap::new();
         for c in window {
             hash_map.insert(c, slice_no);
         }
         if hash_map.len() == window_size {
-            println!("Unique string of length: {window_size} found in slice {slice_no}");
-            println!("First pattern match character occurs at index: {}/{}", 
-                slice_no + window_size, 
-                character_vec.len()
-            );
-            break
-        } else {
-            if slice_no % 128 == 0 {
-                println!("{slice_no} slices checked");
-            }
+            return slice_no + window_size;
         }
     }
+    0
+}
+
+fn main() {
+    let path = Path::new("src/input.txt");
+    let part_1 = 4;
+    let part_2 = 14;
+
+    println!("Part 1: {}", find_answer(path, part_1));
+    println!("Part 2: {}", find_answer(path, part_2));
 }
